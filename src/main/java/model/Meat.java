73662a2e5e3fa6dd6136d91e4b418ff933376a94
GermanGri <german.gribanov@gmail.com>
2023-12-04 21:01:23 +0300

    final

--- src/main/java/model/Meat.java
+++ src/main/java/model/Meat.java
@@ -1,14 +1,16 @@
 package model;
 
+import model.constants.Discount;
+
 public class Meat extends Food implements Discountable {
-    private double discount;
+
     public Meat(int amount, double price){
         this.amount = amount;
         this.price = price;
         this.isVegetarian = false;
     }
     public double getDiscount() {
-        return discount;
+        return Discount.NO_DISCOUNT;
     }
 
 }
